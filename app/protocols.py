32d7abdcb6eae0b105882d85194b9dfac728d8b0
rbagrov <bagrov.rostislav@gmail.com>
2017-03-15 23:18:17 +0200

    I#4 refactor telnet class

--- app/protocols.py
+++ app/protocols.py
@@ -5,3 +5,11 @@ import telnetlib
 from collections import namedtuple
 
 telnet_parameters = namedtuple('telnet_parameters', 'host port login password')
+
+class telnet(object):
+
+    def __init__(self, host):
+        self.t_obj = telnetlib.Telnet(host)
+
+    def connect(self):
+        pass
