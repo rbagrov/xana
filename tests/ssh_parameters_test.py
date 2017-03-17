1f16d194ba78ec8ef50959dc37833ed8d5348c38
rbagrov <bagrov.rostislav@gmail.com>
2017-03-17 17:58:39 +0200

    Fix typo in ssh test

--- tests/ssh_parameters_test.py
+++ tests/ssh_parameters_test.py
@@ -5,4 +5,4 @@ from app.protocols import ssh
 
 def ssh_parameters_test():
     ss = ssh(hostname = '127.0.0.1', port = 22, username='user', password='password')
-    assert(ss.hostname and ss.port, ss.username and ss.password)
+    assert(ss.hostname and ss.port and ss.username and ss.password)
