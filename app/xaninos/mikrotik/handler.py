f45f321adf75a414b650a0b1c44db74a0e666e59
rbagrov <bagrov.rostislav@gmail.com>
2017-03-19 09:26:43 +0200

    I#1 Init

---
+++ app/xaninos/mikrotik/handler.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from app import protocols
+
+def get_config():
+    pass
+
+def _connect():
+    pass
+
+def tag_version():
+    pass
+
+def set_config():
+    pass
+
+def save_config():
+    pass
