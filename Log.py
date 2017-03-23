277d1831ab5b7881c6774449549769a9172b524d
rbagrov <bagrov.rostislav@gmail.com>
2017-03-23 16:26:20 +0200

    I#6-1,2,3 Improve logging

---
+++ Log.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import logging
+import logging.handlers
+
+
+class Debug:
+    enabled: False
+
+if Debug.enabled:
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.INFO)
+else:
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.ERROR)
+
+syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
+
+formatter = logging.Formatter('%(module)s.%(funcName)s: %(message)s')
+
+syslog_handler.setFormatter(formatter)
+
+log.addHandler(syslog_handler)
